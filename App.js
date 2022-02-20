f122086d30d638b77f8da311cda86e019337bd61
Amanda <amanda.wikstrom@hotmail.com>
2022-02-20 12:42:54 +0100

    init project and config homepage/App.js

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>
+        CityPop</Text>
+
+      <Pressable style={styles.searchBtn}>
+        <Text>SEARCH BY CITY</Text>
+      </Pressable>
+
+      <Pressable style={styles.searchBtn}>
+        <Text>SEARCH BY COUNTRY</Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  searchBtn: {
+    width: '98%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: 'gray',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 2,
+  },
+  title: {
+    fontSize: 28,
+    marginBottom: 50,
+  }
 });
