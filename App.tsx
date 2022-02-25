addbb4cc3211f5ddae20bfa3d84f22d9ccc6143e
Amanda <amanda.wikstrom@hotmail.com>
2022-02-25 16:25:25 +0100

    add StatusBar component

--- App.tsx
+++ App.tsx
@@ -1,41 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import HomePage from './src/pages/Home';
-import Country from './src/pages/Country';
+import SearchCity from './src/pages/SearchCity';
 import City from './src/pages/City';
+import SearchCountry from './src/pages/SearchCountry';
+import Country from './src/pages/Country';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-import SearchCity from './src/pages/SearchCity';
-import SearchCountry from './src/pages/SearchCountry';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='CityPop'
-          component={HomePage}
-        />
-        <Stack.Screen
-          name='Search City'
-          component={SearchCity}
-        />
-        <Stack.Screen
-          name='Search Country'
-          component={SearchCountry}
-        />
-        <Stack.Screen
-          name='City'
-          component={City}
-        />
-        <Stack.Screen
-          name='Country'
-          component={Country}
-        />
-      </Stack.Navigator>
-      { /* <React.Fragment> <StatusBar style="auto" /></React.Fragment> */}
-    </NavigationContainer>
+    <React.Fragment>
+      <StatusBar style="auto" />
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='CityPop'
+            component={HomePage}
+          />
+          <Stack.Screen
+            name='Search City'
+            component={SearchCity}
+          />
+          <Stack.Screen
+            name='Search Country'
+            component={SearchCountry}
+          />
+          <Stack.Screen
+            name='City'
+            component={City}
+          />
+          <Stack.Screen
+            name='Country'
+            component={Country}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </React.Fragment>
   );
 }
